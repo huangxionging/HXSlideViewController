6fe9ae99bcd76dea6c18020c12e632e9dbaaa4bc
huangxiong <huangxionging@163.com>
2015-08-04 23:44:29 +0800

    Initial Commit

---
+++ HXSlideViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXSlideViewController
+//
+//  Created by huangxiong on 15/8/4.
+//  Copyright (c) 2015年 huangxiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXSlideViewController/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HXSlideViewController
+//
+//  Created by huangxiong on 15/8/4.
+//  Copyright (c) 2015年 huangxiong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
